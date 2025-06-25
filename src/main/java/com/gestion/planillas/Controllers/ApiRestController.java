245d69480ddac68227ce03c9739cebafba4c6dc6
marvin-datalysis <marvin.cubias@datalysisgroup.com>
2025-06-24 22:14:43 -0600

    validacion de token add to planilla

--- src/main/java/com/gestion/planillas/Controllers/ApiRestController.java
+++ src/main/java/com/gestion/planillas/Controllers/ApiRestController.java
@@ -6,15 +6,20 @@ import com.gestion.planillas.DAO.usuarioDAO;
 import com.gestion.planillas.DAO.contaduriaDAO;
 import com.gestion.planillas.modelos.Permiso;
 import com.gestion.planillas.modelos.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
 import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
 import java.text.DateFormat;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
@@ -38,69 +43,91 @@ public class ApiRestController {
     @Autowired
     private contaduriaDAO contaduriaDAO;
 
-    Long ACCES_TOKEN_VALIDITY_SECONDS = 2_592_000L;
+    Long ACCES_TOKEN_VALIDITY_SECONDS = 21_600L;
     String ACCES_TOKEN_SECRET = "7WK5T79u5mIzjIXXi2oI9Fglmgivv7RAJ7izyj9tUyQ";
 
     @PostMapping("/login")
-    public String login(@RequestBody Map<String, String> body) {
+    public ResponseEntity<?> login(@RequestBody Map<String, String> body) {
         String username = body.get("username");
         String password = body.get("password");
 
         if (username == null || password == null) {
-            return "Faltan datos de login";
+            return ResponseEntity
+                    .badRequest()
+                    .body(Map.of("Error","Debe proporcionar un nombre de usuario y una contraseña"));
         }
 
         Usuario usuario = usuarioDAO.getUsuarioPorCampo("username", username);
         if (usuario == null) {
-            return "El usuario no existe";
+            return ResponseEntity
+                    .badRequest()
+                    .body(Map.of("Error","El usuario no existe"));
         }
 
         if (!passwordEncoder.matches(password, usuario.getPassword())) {
-            return "Contraseña incorrecta";
+            return ResponseEntity
+                    .badRequest()
+                    .body(Map.of("Error","Contraseña incorrecta"));
         }
 
-        long expirationTime= ACCES_TOKEN_VALIDITY_SECONDS *1_000;
-        Date expirationDate=new Date(System.currentTimeMillis()+expirationTime);
+        long expirationTime = ACCES_TOKEN_VALIDITY_SECONDS * 1_000;
+        Date expirationDate = new Date(System.currentTimeMillis() + expirationTime);
 
         List<Permiso> permisos = usuarioDAO.getPermisosDeUsuario(username);
         List<String> nombres = permisos.stream()
                 .map(Permiso::getNombrePermiso)
                 .collect(Collectors.toList());
 
-        Map<String,Object> extra=new HashMap<>();
+        Map<String, Object> extra = new HashMap<>();
         extra.put("permisos", nombres);
 
-        return Jwts.builder()
+        String token= Jwts.builder()
                 .setSubject(username)
                 .setExpiration(expirationDate)
                 .addClaims(extra)
                 .signWith(Keys.hmacShaKeyFor(ACCES_TOKEN_SECRET.getBytes()))
                 .compact();
+        return ResponseEntity
+                .ok(Map.of("Token",token));
     }
 
     @PostMapping("/planilla")
     public ResponseEntity<?> planillaPost(@RequestBody(required = false) Map<String, String> body,
-                                          @RequestHeader(value = "Authorization", required = false) String authHeader){
-            /*if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of(
-                        "error", "Token no proporcionado o formato inválido"
-                ));
-            }
+                                          @RequestHeader(value = "Authorization", required = false) String authHeader) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of(
+                    "error", "No se proporcionó el token"
+            ));
+        }
 
-            String token = authHeader.substring(7); // quitar "Bearer "
-            Claims claims;
+        String token = authHeader.substring(7); // quitar "Bearer "
+        Claims claims;
 
-            try {
-                claims = Jwts.parserBuilder()
-                        .setSigningKey(Keys.hmacShaKeyFor(ACCES_TOKEN_SECRET.getBytes()))
-                        .build()
-                        .parseClaimsJws(token)
-                        .getBody();
-            } catch (JwtException e) {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of(
-                        "error", "Token inválido: " + e.getMessage()
-                ));
-            }*/
+        try {
+            claims = Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(ACCES_TOKEN_SECRET.getBytes()))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        }
+        catch (ExpiredJwtException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of(
+                    "Error", "El token está expirado"
+            ));
+        }
+        catch (JwtException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of(
+                    "Error", "El token proporcionado no es válido"
+            ));
+        }
+
+        String[] permisos= (String[]) claims.get("permisos");
+        if (!Arrays.asList(permisos).contains("ROLE_Ver_planilla")){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of(
+                    "Error", "No tiene el permiso necesario para ver la planilla"
+            ));
+        }
+
         if (body == null || body.isEmpty()) {
             return ResponseEntity.badRequest().body(Map.of(
                     "Error", "El cuerpo de la solicitud no puede estar vacío"
@@ -111,7 +138,7 @@ public class ApiRestController {
         String fecha_fin = body.get("fecha_fin");
         int empleado_id;
 
-        if(fecha_inicio==null||fecha_fin==null){
+        if (fecha_inicio == null || fecha_fin == null) {
             return ResponseEntity
                     .badRequest()
                     .body(Map.of("Error", "Se debe enviar una fecha de inicio y una fecha de fin"));
@@ -120,9 +147,9 @@ public class ApiRestController {
         try {
             DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
             formatter.setLenient(false);
-            Date fecha_inicio_date=formatter.parse(fecha_inicio);
-            Date fecha_fin_date=formatter.parse(fecha_fin);
-            if (fecha_fin_date.before(fecha_inicio_date)){
+            Date fecha_inicio_date = formatter.parse(fecha_inicio);
+            Date fecha_fin_date = formatter.parse(fecha_fin);
+            if (fecha_fin_date.before(fecha_inicio_date)) {
                 return ResponseEntity
                         .badRequest()
                         .body(Map.of("Error", "La fecha de inicio debe ser menor que la fecha de fin"));
@@ -133,26 +160,26 @@ public class ApiRestController {
                     .body(Map.of("Error", "Las fechas no son válidas"));
         }
 
-        List<Object[]> planillaList= new ArrayList<>();
+        List<Object[]> planillaList = new ArrayList<>();
 
-        if (body.get("empleado_id")!=null){
-            try{
+        if (body.get("empleado_id") != null) {
+            try {
                 empleado_id = Integer.parseInt(body.get("empleado_id"));
-            } catch (Exception e){
+            } catch (Exception e) {
                 return ResponseEntity
                         .badRequest()
                         .body(Map.of("Error", "El id del empleado debe ser un número o una cadena"));
             }
-            Object[] boleta= contaduriaDAO.planillaEmpleado(fecha_inicio,fecha_fin,empleado_id);
-            if(boleta == null){
+            Object[] boleta = contaduriaDAO.planillaEmpleado(fecha_inicio, fecha_fin, empleado_id);
+            if (boleta == null) {
                 return ResponseEntity
                         .status(HttpStatus.NOT_FOUND)
                         .body(Map.of("Error", "No se encontró el empleado especificado"));
             }
             planillaList.add(boleta);
-        }else{
-            planillaList= contaduriaDAO.planilla(fecha_inicio,fecha_fin);
-            if(planillaList.isEmpty()){
+        } else {
+            planillaList = contaduriaDAO.planilla(fecha_inicio, fecha_fin);
+            if (planillaList.isEmpty()) {
                 return ResponseEntity
                         .status(HttpStatus.NOT_FOUND)
                         .body(Map.of("Error", "No se encontraron empleados en planilla para las fechas dadas"));
@@ -168,18 +195,18 @@ public class ApiRestController {
             resultado.put("Cargo", planilla[2]);
             resultado.put("Salario / hora", currencyFormatter.format(planilla[3]));
             resultado.put("Horas normales", currencyFormatter.format(planilla[4]));
-            resultado.put("Salario base",currencyFormatter.format(planilla[5]));
+            resultado.put("Salario base", currencyFormatter.format(planilla[5]));
             resultado.put("Vacaciones", currencyFormatter.format(planilla[6]));
-            resultado.put("Horas extra",currencyFormatter.format(planilla[7]));
-            resultado.put("Permisos remunerables",currencyFormatter.format(planilla[8]));
+            resultado.put("Horas extra", currencyFormatter.format(planilla[7]));
+            resultado.put("Permisos remunerables", currencyFormatter.format(planilla[8]));
             resultado.put("Otros beneficios", currencyFormatter.format(planilla[9]));
             resultado.put("Salario + beneficios", currencyFormatter.format(planilla[10]));
             resultado.put("ISSS", currencyFormatter.format(planilla[11]));
-            resultado.put("AFP",currencyFormatter.format(planilla[12]));
-            resultado.put("ISR",currencyFormatter.format(planilla[13]));
+            resultado.put("AFP", currencyFormatter.format(planilla[12]));
+            resultado.put("ISR", currencyFormatter.format(planilla[13]));
             resultado.put("Otras deducciones", currencyFormatter.format(planilla[14]));
-            resultado.put("Total deducciones",currencyFormatter.format(planilla[15]));
-            resultado.put("Salario neto",currencyFormatter.format(planilla[16]));
+            resultado.put("Total deducciones", currencyFormatter.format(planilla[15]));
+            resultado.put("Salario neto", currencyFormatter.format(planilla[16]));
             resultados.add(resultado);
         }
         ObjectMapper mapper = new ObjectMapper();
@@ -217,33 +244,33 @@ public class ApiRestController {
         Pattern pattern = Pattern.compile("(?=.*[0-9]).+");//al menos un numero
         Matcher matcher = pattern.matcher(password);
 
-        List<String> Errores=new ArrayList<>();
-        if(!matcher.matches())
+        List<String> Errores = new ArrayList<>();
+        if (!matcher.matches())
             Errores.add("La contraseña debe tener al menos un número");
 
         pattern = Pattern.compile("(?=.*[a-zA-Z]).+");//al menos una letra
         matcher = pattern.matcher(password);
-        if(!matcher.matches())
+        if (!matcher.matches())
             Errores.add("La contraseña debe tener al menos una letra");
 
         pattern = Pattern.compile("(?=.*[@#$%^&+=*_/]).+");//al menos un simbolo
         matcher = pattern.matcher(password);
-        if(!matcher.matches())
+        if (!matcher.matches())
             Errores.add("La contraseña debe tener al menos un símbolo especial");
 
-        if(password.length()<8)
+        if (password.length() < 8)
             Errores.add("La contraseña debe tener al menos 8 caracteres");
 
         if (!Errores.isEmpty())
-            return ResponseEntity.badRequest().body(Map.of("Errores",Errores));
+            return ResponseEntity.badRequest().body(Map.of("Errores", Errores));
 
-        Usuario usuario=new Usuario();
+        Usuario usuario = new Usuario();
         usuario.setUsername(username);
         usuario.setEmail(email);
         usuario.setPassword(passwordEncoder.encode(password));
 
         usuarioDAO.guardarUsuario(usuario);
 
-        return ResponseEntity.ok().body(Map.of("Message","Usuario creado exitosamente"));
+        return ResponseEntity.ok().body(Map.of("Message", "Usuario creado exitosamente"));
     }
 }
